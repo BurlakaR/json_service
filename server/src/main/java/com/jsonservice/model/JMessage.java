387f3ddf2a3183b219bc45e90215ade63294bf16
BurlakaR <bugaga1998@gmail.com>
2018-02-21 13:51:50 +0200

    rbCommit_lombook+

--- server/src/main/java/com/jsonservice/model/JMessage.java
+++ server/src/main/java/com/jsonservice/model/JMessage.java
@@ -1,5 +1,9 @@
 package com.jsonservice.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -9,34 +13,17 @@ public class JMessage implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
-    private int id;
+    @Getter(AccessLevel.PUBLIC) @Setter (AccessLevel.PUBLIC) private int id;
 
     @Column(name = "text")
-    private String text;
+    @Getter(AccessLevel.PUBLIC) @Setter (AccessLevel.PUBLIC)private String text;
 
-    public JMessage() {
-    }
 
     public JMessage(String text) {
         this.text = text;
     }
 
 
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     @Override
     public String toString() {
